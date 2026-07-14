63e4c9baf70494f01da2b3eb363c37c3a47b1857
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-07-14 12:37:18 +0000

    ajout de la page de connexion

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,23 @@
 /* jquery */
 
 let inscriptionbutton=$("#inscription");
+let connexionbutton=$("#connexion");
 let modal = $("#modal");
+let loginconnexion = $(".login-connexion");
 let section = $(".section");
 
 console.log(inscription);
 
 inscriptionbutton.click(() =>{
     modal.css({"display":"block"});
+    loginconnexion.css({"display":"none"});
     section.css({"filter":"blur(2px)"});
 
 })
+
+connexionbutton.click(() =>{
+    loginconnexion.css({"display":"block"});
+    modal.css({"display":"none"});
+    section.css({"filter":"blur(2px)"});
+})
+        

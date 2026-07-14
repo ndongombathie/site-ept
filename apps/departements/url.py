5ee4a48daa029cb2e65b3afb9d69bf1667ab52c2
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-07-14 21:17:42 +0000

    suppression des commentaires dans le fichier css

--- apps/departements/url.py
+++ apps/departements/url.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import git
+from .views import git,gem,gc,tc
 
 
 urlpatterns = [
-    path("git/",git,name="git")
+    path("git/",git,name="git"),
+    path("gem/",gem,name="gem"),
+    path("gc/",gc,name="gc"),
+    path("tc/",tc,name="tc")
 ]
--- apps/departements/views.py
+++ apps/departements/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def git(request):
     return render(request,"git.html")
+
+def gem(request):
+    return render(request,"gem.html")
+
+def gc(request):
+    return render(request,"gc.html")
+
+def tc(request):
+    return render(request,"tc.html")
